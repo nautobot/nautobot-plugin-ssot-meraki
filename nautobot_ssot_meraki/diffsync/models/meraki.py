4956d017b61637a354a3d0fa4f3ae2c9319a20bc
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-09-26 11:56:05 -0500

    feat: ✨ Add MerakiNetwork model and correct docstrings for MerakiDevice

--- nautobot_ssot_meraki/diffsync/models/meraki.py
+++ nautobot_ssot_meraki/diffsync/models/meraki.py
@@ -1,6 +1,23 @@
 """Nautobot SSoT for Meraki DiffSync models for Nautobot SSoT for Meraki SSoT."""
 
-from nautobot_ssot_meraki.diffsync.models.base import Device
+from nautobot_ssot_meraki.diffsync.models.base import Device, Network
+
+
+class MerakiNetwork(Network):
+    """Meraki implementation of Network DiffSync model."""
+
+    @classmethod
+    def create(cls, diffsync, ids, attrs):
+        """Create Network in Meraki from MerakiNetwork object."""
+        return super().create(diffsync=diffsync, ids=ids, attrs=attrs)
+
+    def update(self, attrs):
+        """Update Network in Meraki from MerakiNetwork object."""
+        return super().update(attrs)
+
+    def delete(self):
+        """Delete Network in Meraki from MerakiNetwork object."""
+        return self
 
 
 class MerakiDevice(Device):
@@ -8,13 +25,13 @@ class MerakiDevice(Device):
 
     @classmethod
     def create(cls, diffsync, ids, attrs):
-        """Create Device in Meraki from NautobotSsotMerakiDevice object."""
+        """Create Device in Meraki from MerakiDevice object."""
         return super().create(diffsync=diffsync, ids=ids, attrs=attrs)
 
     def update(self, attrs):
-        """Update Device in Meraki from NautobotSsotMerakiDevice object."""
+        """Update Device in Meraki from MerakiDevice object."""
         return super().update(attrs)
 
     def delete(self):
-        """Delete Device in Meraki from NautobotSsotMerakiDevice object."""
+        """Delete Device in Meraki from MerakiDevice object."""
         return self
