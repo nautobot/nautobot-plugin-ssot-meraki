a9259161c24cf0513f51f26139f401b1c3564af5
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-09-26 14:53:10 -0500

    fix: 🐛 Update Job to use DashboardClient and use PLUGIN_CFG settings

--- nautobot_ssot_meraki/jobs.py
+++ nautobot_ssot_meraki/jobs.py
@@ -1,10 +1,12 @@
 """Jobs for Meraki SSoT integration."""
-
-from diffsync import DiffSyncFlags
+from django.conf import settings
 from nautobot.extras.jobs import BooleanVar, Job
-from nautobot_ssot.jobs.base import DataSource, DataTarget
+from nautobot_ssot.jobs.base import DataSource
 from nautobot_ssot_meraki.diffsync.adapters import meraki, nautobot
+from nautobot_ssot_meraki.utils.meraki import DashboardClient
 
+
+PLUGIN_CFG = settings.PLUGINS_CONFIG["nautobot_ssot_meraki"]
 
 name = "Meraki SSoT"  # pylint: disable=invalid-name
 
@@ -14,15 +16,10 @@ class MerakiDataSource(DataSource, Job):
 
     debug = BooleanVar(description="Enable for more verbose debug logging", default=False)
 
-    def __init__(self):
-        """Initialize Meraki Data Source."""
-        super().__init__()
-        self.diffsync_flags = self.diffsync_flags | DiffSyncFlags.CONTINUE_ON_FAILURE
-
     class Meta:  # pylint: disable=too-few-public-methods
         """Meta data for Meraki."""
 
-        name = "Meraki to Nautobot"
+        name = "Meraki => Nautobot"
         data_source = "Meraki"
         data_target = "Nautobot"
         description = "Sync information from Meraki to Nautobot"
@@ -39,52 +36,11 @@ class MerakiDataSource(DataSource, Job):
 
     def load_source_adapter(self):
         """Load data from Meraki into DiffSync models."""
-        self.source_adapter = meraki.MerakiAdapter(job=self, sync=self.sync)
+        client = DashboardClient(logger=self, org_id=PLUGIN_CFG["meraki_org_id"], token=PLUGIN_CFG["meraki_token"])
+        self.source_adapter = meraki.MerakiAdapter(job=self, sync=self.sync, client=client)
         self.source_adapter.load()
 
     def load_target_adapter(self):
         """Load data from Nautobot into DiffSync models."""
         self.target_adapter = nautobot.NautobotAdapter(job=self, sync=self.sync)
         self.target_adapter.load()
-
-
-class MerakiDataTarget(DataTarget, Job):
-    """Meraki SSoT Data Target."""
-
-    debug = BooleanVar(description="Enable for more verbose debug logging", default=False)
-
-    def __init__(self):
-        """Initialize Meraki Data Target."""
-        super().__init__()
-        self.diffsync_flags = self.diffsync_flags | DiffSyncFlags.CONTINUE_ON_FAILURE
-
-    class Meta:  # pylint: disable=too-few-public-methods
-        """Meta data for Meraki."""
-
-        name = "Nautobot to Meraki"
-        data_source = "Nautobot"
-        data_target = "Meraki"
-        description = "Sync information from Nautobot to Meraki"
-
-    @classmethod
-    def config_information(cls):
-        """Dictionary describing the configuration of this DataTarget."""
-        return {}
-
-    @classmethod
-    def data_mappings(cls):
-        """List describing the data mappings involved in this DataSource."""
-        return ()
-
-    def load_source_adapter(self):
-        """Load data from Nautobot into DiffSync models."""
-        self.source_adapter = nautobot.NautobotAdapter(job=self, sync=self.sync)
-        self.source_adapter.load()
-
-    def load_target_adapter(self):
-        """Load data from Meraki into DiffSync models."""
-        self.target_adapter = meraki.MerakiAdapter(job=self, sync=self.sync)
-        self.target_adapter.load()
-
-
-jobs = [MerakiDataSource, MerakiDataTarget]
