e02facdb0ca9ef7b400f17e540dfc2e166c957d8
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-09-27 13:24:01 -0500

    feat: ✨ Add OS Version CustomField to be created in database ready signal.

--- nautobot_ssot_meraki/__init__.py
+++ nautobot_ssot_meraki/__init__.py
@@ -1,14 +1,10 @@
 """Plugin declaration for nautobot_ssot_meraki."""
-# Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
-try:
-    from importlib import metadata
-except ImportError:
-    # Python version < 3.8
-    import importlib_metadata as metadata
+from importlib import metadata
+from nautobot.core.signals import nautobot_database_ready
+from nautobot.extras.plugins import NautobotAppConfig
+from nautobot_ssot_meraki.signals import nautobot_database_ready_callback
 
 __version__ = metadata.version(__name__)
-
-from nautobot.extras.plugins import NautobotAppConfig
 
 
 class NautobotSsotMerakiConfig(NautobotAppConfig):
@@ -25,6 +21,12 @@ class NautobotSsotMerakiConfig(NautobotAppConfig):
     max_version = "1.9999"
     default_settings = {}
     caching_config = {}
+
+    def ready(self):
+        """Trigger callback when database is ready."""
+        super().ready()
+
+        nautobot_database_ready.connect(nautobot_database_ready_callback, sender=self)
 
 
 config = NautobotSsotMerakiConfig  # pylint:disable=invalid-name
