1e9bf0218253b59b78c92021d43ee1e76fe40452
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-09-27 12:41:32 -0500

    feat: ✨ Add load_devices function to Meraki adapter.

--- nautobot_ssot_meraki/diffsync/adapters/meraki.py
+++ nautobot_ssot_meraki/diffsync/adapters/meraki.py
@@ -1,8 +1,8 @@
 """Nautobot SSoT for Meraki Adapter for Meraki SSoT plugin."""
-
 from diffsync import DiffSync
 from diffsync.exceptions import ObjectNotFound
 from nautobot_ssot_meraki.diffsync.models.meraki import MerakiNetwork, MerakiDevice
+from nautobot_ssot_meraki.utils.meraki import parse_hostname_for_role
 
 
 class MerakiAdapter(DiffSync):
@@ -49,9 +49,40 @@ class MerakiAdapter(DiffSync):
                 )
                 self.add(new_network)
 
+    def load_devices(self):
+        """Load devices from Meraki dashboard into DiffSync models."""
+        if self.tenant:
+            tenant = self.tenant.name
+        else:
+            tenant = None
+        statuses = self.conn.get_device_statuses()
+        status = "Offline"
+        for dev in self.conn.get_org_devices():
+            if dev["name"] in statuses:
+                if statuses[dev["name"]]["status"] == "online":
+                    status = "Active"
+            try:
+                self.get(self.device, dev["name"])
+                self.job.log_warning(message=f"Duplicate device {dev['name']} found and being skipped.")
+            except ObjectNotFound:
+                print(f"Network map: {self.conn.network_map}")
+                new_dev = self.device(
+                    name=dev["name"],
+                    notes=dev["notes"],
+                    serial=dev["serial"],
+                    status=status,
+                    role=parse_hostname_for_role(dev_hostname=dev["name"]),
+                    model=dev["model"],
+                    network=self.conn.network_map[dev["networkId"]]["name"],
+                    tenant=tenant,
+                    uuid=None,
+                )
+                self.add(new_dev)
+
     def load(self):
         """Load data from Meraki into DiffSync models."""
         if self.conn.validate_organization_exists():
             self.load_networks()
+            self.load_devices()
         else:
             self.job.log_failure(message="Specified organization ID not found in Meraki dashboard.")
