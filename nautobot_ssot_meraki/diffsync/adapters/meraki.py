34deaa360ad55c52c4e1660d357622eed9020c3a
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-09-27 18:06:37 -0500

    feat: ✨ Add function to load management ports for devices.

--- nautobot_ssot_meraki/diffsync/adapters/meraki.py
+++ nautobot_ssot_meraki/diffsync/adapters/meraki.py
@@ -1,7 +1,7 @@
 """Nautobot SSoT for Meraki Adapter for Meraki SSoT plugin."""
 from diffsync import DiffSync
 from diffsync.exceptions import ObjectNotFound
-from nautobot_ssot_meraki.diffsync.models.meraki import MerakiNetwork, MerakiDevice
+from nautobot_ssot_meraki.diffsync.models.meraki import MerakiNetwork, MerakiDevice, MerakiPort
 from nautobot_ssot_meraki.utils.meraki import parse_hostname_for_role
 
 
@@ -10,8 +10,9 @@ class MerakiAdapter(DiffSync):
 
     network = MerakiNetwork
     device = MerakiDevice
+    port = MerakiPort
 
-    top_level = ["network", "device"]
+    top_level = ["network", "device", "port"]
 
     def __init__(self, job, sync, client, tenant=None, *args, **kwargs):
         """Initialize Meraki.
@@ -81,8 +82,37 @@ class MerakiAdapter(DiffSync):
                         version=dev["firmware"],
                     )
                     self.add(new_dev)
+                    self.load_management_port(device_name=dev["name"], serial=dev["serial"])
             else:
                 self.job.log_warning(message=f"Device serial {dev['serial']} is missing hostname so will be skipped.")
+
+    def load_management_port(self, device_name: str, serial: str):
+        """Load management port of a device from Meraki dashboard into DiffSync models."""
+        mgmt_port_names = self.conn.get_management_port_names(serial=serial)
+        uplink_settings = self.conn.get_uplink_settings(serial=serial)
+        uplink_statuses = self.conn.get_org_uplink_statuses()
+
+        for port in mgmt_port_names:
+            try:
+                self.get(self.port, {"name": port, "device": device_name})
+            except ObjectNotFound:
+                uplink_status = "Planned"
+                if self.device_map[device_name]["networkId"] in uplink_statuses:
+                    uplinks = uplink_statuses[self.device_map[device_name]["networkId"]]["uplinks"]
+                    for link in uplinks:
+                        if link["interface"] == port and link["status"] == "active":
+                            uplink_status = "Active"
+                new_port = self.port(
+                    name=port,
+                    device=device_name,
+                    management=True,
+                    enabled=uplink_settings[port]["enabled"],
+                    port_type="1000base-t",
+                    port_status=uplink_status,
+                    tagging=uplink_settings[port]["vlanTagging"]["enabled"],
+                    uuid=None,
+                )
+                self.add(new_port)
 
     def load(self):
         """Load data from Meraki into DiffSync models."""
