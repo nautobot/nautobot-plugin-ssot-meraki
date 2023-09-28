2831fafefd25ad4bebeb246e168a30e561a4e7b3
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-09-28 14:54:58 -0500

    test: ✅ Add expected fixture for get_management_port_names()

--- nautobot_ssot_meraki/tests/fixtures/fixtures.py
+++ nautobot_ssot_meraki/tests/fixtures/fixtures.py
@@ -13,6 +13,9 @@ GET_ORG_NETWORKS_RECV_FIXTURE = load_json("./nautobot_ssot_meraki/tests/fixtures
 NETWORK_MAP_FIXTURE = load_json("./nautobot_ssot_meraki/tests/fixtures/network_map.json")
 GET_ORG_DEVICES_FIXTURE = load_json("./nautobot_ssot_meraki/tests/fixtures/get_org_devices.json")
 GET_DEVICE_STATUSES_FIXTURE = load_json("./nautobot_ssot_meraki/tests/fixtures/get_device_statuses.json")
+GET_MANAGEMENT_PORT_NAMES_RECV_FIXTURE = load_json(
+    "./nautobot_ssot_meraki/tests/fixtures/get_management_port_names_recv.json"
+)
 GET_MANAGEMENT_PORT_NAMES_SENT_FIXTURE = load_json(
     "./nautobot_ssot_meraki/tests/fixtures/get_management_port_names_sent.json"
 )
